597f680d1d4ed9ee7c6d767a24944004b9a583a2
RimonChakma <rimon.chakma880@gmail.com>
2025-04-02 19:10:31 +0600

    create addTask

--- lib/task_cubit.dart
+++ lib/task_cubit.dart
@@ -2,4 +2,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:task_app/task_state.dart';
 class TaskCubit extends Cubit<TaskState>{
   TaskCubit():super(TaskState(task: []));
+  
+  void addTask (String title) {
+    final tasks = List<Map<String,dynamic>>.from(state.task)..add({title:title});
+    emit(TaskState(task: tasks));
+  }
 }
