22847cb9c5971f382a80ca84bba03c47e40cc018
RimonChakma <rimon.chakma880@gmail.com>
2025-04-03 19:47:52 +0600

    task add with ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:task_app/task_cubit.dart';
+import 'package:task_app/task_state.dart';
 
 void main(){
   runApp(MyApp());
@@ -9,19 +12,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return BlocProvider(create: (context) => TaskCubit(),child: MaterialApp(
       debugShowCheckedModeBanner: false,
       home: TaskScreen(),
+    ),
     );
   }
 }
 
 
-
 class TaskScreen extends StatelessWidget {
   const TaskScreen({super.key});
-
-
   @override
   Widget build(BuildContext context) {
 
@@ -41,10 +42,27 @@ class TaskScreen extends StatelessWidget {
                   hintText: "add task"
               ),),),
               SizedBox(width:10,),
-              ElevatedButton(onPressed: (){}, child: Icon(Icons.check))
+              ElevatedButton(onPressed: (){
+                context.read<TaskCubit>().addTask(taskController.text.toString());
+              }, child: Icon(Icons.check)),
+              SizedBox(height: 10,),
+
             ],
           ),
-          
+
+          Expanded(
+            child: BlocBuilder <TaskCubit,TaskState> (builder: (context, state) {
+              return ListView.builder(
+                itemCount: state.task.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(state.task[index]["title"].toString()),
+                  );
+                },);
+            }
+            ),
+          )
+
         ],
       ),),
     );
--- lib/task_cubit.dart
+++ lib/task_cubit.dart
@@ -2,9 +2,12 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:task_app/task_state.dart';
 class TaskCubit extends Cubit<TaskState>{
   TaskCubit():super(TaskState(task: []));
-  
-  void addTask (String title) {
-    final tasks = List<Map<String,dynamic>>.from(state.task)..add({title:title});
-    emit(TaskState(task: tasks));
+
+  void addTask(String title) {
+    if (title.isNotEmpty) {
+      final newTask = {'title': title};
+      final updatedTasks = List<Map<String, dynamic>>.from(state.task)..add(newTask);
+      emit(TaskState(task: updatedTasks));
+    }
   }
 }
